ebafd4bc1ff9f89d9ad7fe0c6842e45e4effef8c
thiago-abc <thiagoabc@gmail.com>
2024-08-24 21:21:13 -0300

    correcao de links e funcionalidades

--- meuapp/views.py
+++ meuapp/views.py
@@ -37,7 +37,7 @@ def editar_artista(request, pk):
 def detalhes(request, id):
     meuartista = get_object_or_404(Artista, id=id)
     albuns = Album.objects.filter(artista=meuartista)
-    template = loader.get_template('detalhes.html')
+    #template = loader.get_template('detalhes.html')
 
     return render(request, 'detalhes.html', {
         'meuartista': meuartista,
@@ -65,7 +65,7 @@ def adiciona_artista(request):
         form = ArtistaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('meus_artistas')
+            return redirect('meusartistas')
     else:
         form = ArtistaForm()
     return render(request, 'artista_form.html', {'form': form})
@@ -81,21 +81,4 @@ def adiciona_album(request):
 
     return render(request, 'adicionar_album.html', {'form': form})
 
-"""@api_view(['GET'])
-def genero_data(request):
-    response = requests.get('https://binaryjazz.us/wp-json/genrenator/v1/genre/')
-    if response.status_code == 200:
-        data = response.json()
-        return Response(data)
-    else:
-        return Response({'error': 'Não foi possível obter os dados.'}, status=response.status_code)
-
-def genero_api(request):
-    response = requests.get('http://localhost:8000/api/genero/')
-    if response.status_code == 200:
-        data = response.json()
-    else:
-        data = {'error': 'Não foi possível obter os dados.'}
-
-    return render(request, 'main.html', {'genero': data})"""
 
